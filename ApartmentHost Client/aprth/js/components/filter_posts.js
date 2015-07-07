8f70cfe8c3d2463a115fe637e2207c567415069c
koala <makeag@gmail.com>
2015-07-07 09:33:32 +0300

    Revert "порядок в стилях плюс кликабильность в списке объявлений"
    This reverts commit 274e1a5277b970fd1fec8784b6bfd47cd8afe6fe.

--- ApartmentHost Client/aprth/js/components/filter_posts.js
+++ ApartmentHost Client/aprth/js/components/filter_posts.js
@@ -200,20 +200,20 @@ var PostsFilter = React.createClass({
 		var classesAdrInput = cAdrInput({
 			"w-input": true,
 			"u-form-field": true,
-			"has-error": this.state.noAdressFilterSpecified	
+			"errstate": this.state.noAdressFilterSpecified	
 		});
 		var cDateInput = React.addons.classSet;
 		var classesDateInputFrom = cDateInput({
 			"w-input": true,
 			"u-form-field": true,
 			"rel": true,
-			"has-error": this.state.noDateFromFilterSpecified
+			"errstate": this.state.noDateFromFilterSpecified
 		});
 		var classesDateInputTo = cDateInput({
 			"w-input": true,
 			"u-form-field": true,
 			"rel": true,
-			"has-error": this.state.noDateToFilterSpecified
+			"errstate": this.state.noDateToFilterSpecified
 		});		
 		//представление фильтра
 		return (
