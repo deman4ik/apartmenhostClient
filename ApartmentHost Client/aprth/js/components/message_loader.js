4c3a668ce3aac0511f59be5d3a6d4cb56312d040
koala <makeag@gmail.com>
2015-07-22 18:37:13 +0300

    индикатор загрузки

--- ApartmentHost Client/aprth/js/components/message_loader.js
+++ ApartmentHost Client/aprth/js/components/message_loader.js
@@ -4,29 +4,11 @@
 var Loader = React.createClass({
 	//генерация представления индиктора
 	render: function () {
-		//дополнительный стиль для ячейки таблицы
-		var tdStyle = {paddingRight: "15px"};
 		//генерация индикатора
 		return (
-			<div className="loading-wraper">
-				<center>
-					<div className="panel panel-warning panel-loading">
-						<div className="panel-body bg-warning">
-							<table>
-								<tr>
-									<td style={tdStyle}>
-										<img src="aprth/img/loader.gif"/>
-									</td>
-									<td>
-										<strong className="text-warning loaderBoxTitleText">{this.props.loader.title}</strong>&nbsp;
-										<span className="text-warning loaderBoxBodyText">{this.props.loader.text}</span>
-									</td>
-								</tr>
-							</table>
-						</div>
-					</div>
-				</center>
-			</div>
+			<div className="u-block-loader-bg show">
+				<span className="spinner" title={this.props.loader.title}></span>
+			</div>			
 		);
 	}
 });
