574765eca7509fe7f6a7b93090ed31c1b02cc409
Mimyatina <mim_@mail.ru>
2015-08-28 22:34:20 +0300

    Неизменяемость e-mail в профиле

--- aprth/js/units/profile.js
+++ aprth/js/units/profile.js
@@ -658,6 +658,7 @@ var Profile = React.createClass({
 										type="text"
 										ref="email"
 										id="email"
+										disabled="true"
 										value={this.state.profileTmp.email}
 										placeholder={Utils.getStrResource({lang: this.props.language, code: "UI_PLH_MAIL"})}
 										onChange={this.handleFormItemChange}/>									
