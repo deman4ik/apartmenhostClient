c62c1bd0b5ead64f22b8475fe0361c9424d0efc2
koala <makeag@gmail.com>
2015-10-14 09:15:07 +0300

    поиск на главной

--- aprth/js/units/main.js
+++ aprth/js/units/main.js
@@ -10,7 +10,26 @@ var Main = React.createClass({
 	getInitialState: function () {
 		return {
 			topicsLoaded: false, //признак загруженности статей
-			topics: [] //список статей заглавной страницы
+			topics: [], //список статей заглавной страницы
+			filterClnt: { //текущее состояние фильтра
+				useRadius: "", //искать в радиусе
+				radius: config.searchRadius, //радиус поиска по карте
+				latitude: "", //широта выбранной точки поиска
+				longitude: "", //долгота выбранной точки поиска
+				address: "", //адрес жилья		
+				swLat: "", //широта ЮВ угла квадрата поиска по выбранной точке
+				swLong: "", //долгота ЮВ угла квадрата поиска по выбранной точке
+				neLat: "", //широта СЗ угла квадрата поиска по выбранной точке
+				neLong: "", //долгота СЗ угла квадрата поиска по выбранной точке
+				dFrom: "", //дата начала периода бронирования
+				dTo: "", //дата коночания периода бронирования
+				sex: "", //пол постояльца
+				apartType: "", //тип жилья
+				priceFrom: "", //цена с
+				priceTo: "", //цена по
+				price: "" //цена (сводное состояние)
+			},
+			filter: {} //текущее состояние фильтра	(для сервера)				
 		}
 	},
 	//обработка результатов получения статей заглавной страницы
@@ -47,8 +66,47 @@ var Main = React.createClass({
 	onTopicClick: function (index) {
 		this.context.router.transitionTo("articles", {}, {filter: {name: "HOW_IT_WORKS"}, title: "UI_FOOTER_MENU_HOWITWORKS", convertTitle: true});
 	},
+	//нажатие на поиск
+	onFind: function (find) {
+		this.setState({filterIsSet: true}, Utils.bind(function () {			
+			this.onFindChange(find, this.findAndFilter);
+		}, this));
+	},
+	//нажатие на очистку поиска
+	onFindClear: function () {
+		var tmp = {};
+		_.extend(tmp, this.state.filterClnt);
+		tmp.useRadius = PostsFilterPrms.postFilterUseRadius;
+		tmp.radius = config.searchRadius;
+		tmp.latitude = "";
+		tmp.longitude = "";
+		tmp.address = "";
+		tmp.swLat = "";
+		tmp.swLong = "";
+		tmp.neLat = "";
+		tmp.neLong = "";
+		tmp.dFrom = "";
+		tmp.dTo = "";
+		tmp.sex = "";
+		tmp.apartType = "";
+		tmp.priceFrom = "";
+		tmp.priceTo = "";
+		tmp.price = "";
+		this.setState({filterClnt: tmp, adverts: [], advertsCnt: 0, advertsReady: false, markers: [], filterIsSet: false}, function () {
+			this.saveFilterState();
+		});
+	},
 	//генерация представления главной страницы
 	render: function () {
+		var postsFindForm =	<PostsFindForm language={this.props.language}										
+								latitude={this.state.filterClnt.latitude}
+								longitude={this.state.filterClnt.longitude}
+								address={this.state.filterClnt.address}
+								dFrom={this.state.filterClnt.dFrom}
+								dTo={this.state.filterClnt.dTo}
+								sex={this.state.filterClnt.sex}										
+								onFind={this.onFind}
+								onFindClear={this.onFindClear}/>		
 		//статьи заглавной страницы
 		var topics;
 		if(this.state.topicsLoaded) {
@@ -78,6 +136,9 @@ var Main = React.createClass({
 					<div className="u-block-land">
 						<a className="w-button u-btn-land" href="#">Попробовать</a>
 					</div>
+					<div className="u-block-search-main">
+							{postsFindForm}					
+					</div>					
 				</div>
 				<div className="w-section u-sect-land under">
 					<h1 className="u-t-h1-land2 sub">или</h1>
@@ -88,9 +149,6 @@ var Main = React.createClass({
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/2.jpg"></img></div>
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/3.jpg"></img></div>
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/4.jpg"></img></div>
-						<div className="u-block-land-item center">
-							<br/><span>здесь будет форма поиска<br/></span>
-						</div>
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/5.jpg"></img></div>
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/6.jpg"></img></div>
 						<div className="u-block-land-item"><img calss="u-tmp-img" src="aprth/img/tmp/8.jpg"></img></div>
